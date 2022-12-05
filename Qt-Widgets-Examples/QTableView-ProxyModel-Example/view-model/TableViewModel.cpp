27b7a8627165a54cb3c9e7ccfd66e67920dd45ae
DESKTOP-3N8RAQ7\Сергей <tyulenev.i@yandex.ru>
2022-12-05 12:14:22 +0300

    Add QTableView-ProxyModel-Example

---
+++ Qt-Widgets-Examples/QTableView-ProxyModel-Example/view-model/TableViewModel.cpp
@@ -0,0 +1,83 @@
+﻿#include "TableViewModel.h"
+
+namespace ViewModels {
+
+struct TestModel {
+    int     id;
+    QString name;
+    QString number;
+    bool    status;
+};
+
+TableViewModel::TableViewModel(QObject* parent)
+    : QAbstractTableModel(parent)
+{
+    model = { { 1,  "John",    "10000", true  },
+              { 2,  "Masha",   "11000", false },
+              { 3,  "Ben",     "12000", false },
+              { 4,  "David",   "13000", true  },
+              { 5,  "Lius",    "12300", true  },
+              { 6,  "Bernard", "12340", false },
+              { 45, "Ben",     "12345", false },
+              { 67, "David",   "12346", true  }
+            };
+}
+
+TableViewModel::~TableViewModel()
+{
+
+}
+
+auto TableViewModel::columnCount([[maybe_unused]] const QModelIndex& index) const -> int
+{
+    return 4;
+}
+
+auto TableViewModel::rowCount([[maybe_unused]] const QModelIndex& index) const -> int
+{
+    return model.length();
+}
+
+auto TableViewModel::data(const QModelIndex& index, int role) const -> QVariant
+{
+    if (role == Qt::TextAlignmentRole )
+        return Qt::AlignCenter;
+
+    if (role == Qt::DisplayRole) {
+        switch (index.column()) {
+        case 0: return model.at(index.row()).id;
+        case 1: return model.at(index.row()).name;
+        case 2: return model.at(index.row()).number;
+        case 3: return model.at(index.row()).status;
+        default: assert(!"Should not get here");
+        }
+    }
+
+    return QVariant();
+}
+
+auto TableViewModel::flags(const QModelIndex& index) const -> Qt::ItemFlags
+{
+    return Qt::ItemIsEnabled | Qt::ItemIsSelectable | Qt::ItemIsEditable;
+}
+
+auto TableViewModel::headerData(int section, Qt::Orientation orientation, int role) const -> QVariant
+{
+    if (role == Qt::DisplayRole ) {
+        if (orientation == Qt::Horizontal) {
+            switch (section) {
+            case 0: return "id";
+            case 1: return "name";
+            case 2: return "number";
+            case 3: return "status";
+            [[unlikely]] default: assert(!"Should not get here");
+            }
+        } else [[likely]] {
+            return section;
+        }
+    }
+
+    return QVariant();
+}
+
+} // namespace ViewModels
---
+++ Qt-Widgets-Examples/QTableView-ProxyModel-Example/view-model/TableViewModel.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <QAbstractTableModel>
+
+namespace ViewModels {
+
+struct TestModel;
+
+class TableViewModel : public QAbstractTableModel
+{
+public:
+    explicit TableViewModel(QObject* parent = 0);
+    virtual ~TableViewModel();
+
+    auto columnCount(const QModelIndex& parent) const -> int override;
+    auto rowCount(const QModelIndex& parent) const -> int override;
+    auto data(const QModelIndex& index, int role) const -> QVariant override;
+    auto flags(const QModelIndex& index) const -> Qt::ItemFlags override;
+    auto headerData(int section, Qt::Orientation orientation, int role) const -> QVariant override;
+
+private:
+    QVector<TestModel> model;
+
+};
+
+} // namespace ViewModels
