4b9e6c2572259d1f8e7cc5878b3fba84341b9eb9
DESKTOP-3N8RAQ7\Сергей <tyulenev.i@yandex.ru>
2022-12-14 10:27:27 +0300

    Add QListWidget-Example

---
+++ Qt-Widgets-Examples/QListWidget-Example/CustomWidget.cpp
@@ -0,0 +1,37 @@
+#include "CustomWidget.h"
+#include "ui_CustomWidget.h"
+
+CustomWidget::CustomWidget(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::CustomWidget)
+{
+    ui->setupUi(this);
+
+    ui->title->setStyleSheet("font-size: 14px;"
+                             "font-family: Verdana, Arial, Helvetica, sans-serif;"
+                             "font-weight: bold;"
+                             "color: black;");
+
+    ui->description->setStyleSheet("text-align: justify");
+}
+
+CustomWidget::~CustomWidget()
+{
+    delete ui;
+}
+
+auto CustomWidget::setImage(const QString&& icon) -> void
+{
+    QPixmap outPixmap(icon);
+    ui->image->setPixmap(outPixmap.scaled(128, 128));
+}
+
+auto CustomWidget::setTitle(const QString&& title) -> void
+{
+    ui->title->setText(title);
+}
+
+auto CustomWidget::setDescription(const QString&& description) -> void
+{
+    ui->description->setText(description);
+}
