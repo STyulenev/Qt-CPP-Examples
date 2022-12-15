bf1cf2540915e7f9fac3e16466369075cfc349f0
DESKTOP-3N8RAQ7\Сергей <tyulenev.i@yandex.ru>
2022-12-15 12:40:56 +0300

    Add Boost-Containers-Example

---
+++ Qt-External-Libraries-Examples/Boost-Containers-Example/main.cpp
@@ -0,0 +1,12 @@
+#include "containers-examples/include/BoostSequenceContainer.h"
+
+auto main(int argc, char* argv[]) -> int
+{
+    /// Последовательные контейнеры
+    BoostSequenceContainer::exampleBoostArray();
+    //BoostSequenceContainer::exampleBoostVector();
+    //BoostSequenceContainer::exampleBoostList();
+    //BoostSequenceContainer::exampleBoostDeque();
+
+    return 0;
+}
