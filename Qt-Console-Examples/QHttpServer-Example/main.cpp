31500445cc06af7cc61634cda2a796fb467d7bdf
DESKTOP-3N8RAQ7\Сергей <tyulenev.i@yandex.ru>
2023-01-11 10:20:53 +0300

    Changed QHttpServer-Example

--- Qt-Console-Examples/QHttpServer-Example/main.cpp
+++ Qt-Console-Examples/QHttpServer-Example/main.cpp
@@ -3,63 +3,72 @@
 #include <QtHttpServer/QtHttpServer>
 #include <QtHttpServer/QHttpServerResponder>
 
+#include "userlist.h"
+
 auto main(int argc, char* argv[]) -> int
 {
     QCoreApplication a(argc, argv);
 
     QHttpServer httpServer;
-
     const auto port = httpServer.listen(QHostAddress::Any, 8000);
 
     if (!port)
         return 0;
 
-    qDebug() << QCoreApplication::translate("QHttpServerExample",
-                "Running on http://127.0.0.1:%1/ (Press CTRL+C to quit)").arg(port);
+    qDebug() << QString("Running on http://127.0.0.1:%1/ (Press CTRL+C to quit)").arg(port);
 
-    QJsonArray myData = { 1, 2.2, QString() };
+    UserList users;
 
-    httpServer.route("/myApi", QHttpServerRequest::Method::Get, [&myData](const QHttpServerRequest &request) {
-        QJsonArray array = myData;
-        return QHttpServerResponse(array);
+    httpServer.route("/users", [&users](const QHttpServerRequest& request) {
+        if (request.method() == QHttpServerRequest::Method::Post) { // Добавляет пользователя
+            qDebug() << "url - /users, Post Request";
+
+            auto query = request.query();
+
+            QJsonObject newUser;
+            newUser["name"] = query.queryItemValue("name");
+            newUser["sex"]  = query.queryItemValue("sex");
+            newUser["age"]  = query.queryItemValue("age");
+            users.addUser(newUser);
+
+            QJsonObject status;
+            status["status"] = "OK";
+            return QHttpServerResponse(status);
+
+        } else if (request.method() == QHttpServerRequest::Method::Get) { // Возвращает список пользователя
+            qDebug() << "url - /users, Get Request";
+
+            return QHttpServerResponse(users.getUserList());
+        } else {
+            QJsonObject status;
+            status["status"] = "ERROR";
+            return QHttpServerResponse(status);
+        }
     });
 
-    httpServer.route("/myApi", QHttpServerRequest::Method::Post, [&myData](const QHttpServerRequest &request) {
-        qDebug() << request.value("text");
-        qDebug() << request.value("_id");
+    httpServer.route("/users/<arg>", [&users](int id, const QHttpServerRequest &request) {
+        if (request.method() == QHttpServerRequest::Method::Get) { // Получить пользователя по id
+            qDebug() << "url - /users, Get Request";
 
-        QJsonArray array = myData;
-        return QHttpServerResponse(array);
+            return QHttpServerResponse(users.getUserForId(id));
+        } else if (request.method() == QHttpServerRequest::Method::Delete) { // Удалить пользователя по id
+            qDebug() << "url - /users, Delete Request";
+
+            QJsonObject status;
+            status["status"] = users.deleteUserForId(id) ? "OK" : "ERROR";
+            return QHttpServerResponse(status);
+        } else {
+            QJsonObject status;
+            status["status"] = "ERROR";
+            return QHttpServerResponse(status);
+        }
     });
 
-    /*httpServer.route("/blog/<arg>", QHttpServerRequest::Method::Post, [] (const QHttpServerRequest &req) {
-        qDebug() << "blog";
-
-        qDebug() << req.value("_id");
-        return "error 404";
-    });*/
-
-    httpServer.route("/myApi/<arg>", QHttpServerRequest::Method::Get,
-                     [&myData](int arg, const QHttpServerRequest &request) {
-
-        auto query = request.query();
-        qDebug() << "query =" << query.toString();
-        qDebug() << "id =" << query.queryItemValue("id");
-        qDebug() << "text =" << query.queryItemValue("text");
-
-        qDebug() << "arg = " << arg;
-
-        QJsonArray myApiObject = myData;
-        return QHttpServerResponse(myApiObject);
-    }
-    );
+    httpServer.route("/time", QHttpServerRequest::Method::Get, [](const QHttpServerRequest &request) {
+        QJsonObject status;
+        status["time"] = QTime::currentTime().toString("hh:mm:ss");
+        return QHttpServerResponse(status);
+    });
 
     return a.exec();
 }
-
-
-/*if (req.method() == QHttpServerRequest::Method::Get)
-            return "12";
-
-        if (req.method() == QHttpServerRequest::Method::Post) {
-            //qDebug() << req.value("text");*/
