54a2672af5f82efb26e69febac6c21cac92875bf
DESKTOP-3N8RAQ7\Сергей <tyulenev.i@yandex.ru>
2023-04-21 16:01:27 +0300

    Added QTableView-QSqlDatabase-Example.

---
+++ Qt-Widgets-Examples/QTableView-QSqlDatabase-Example/src/view-model/include/CustomersViewModel.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <QAbstractTableModel>
+#include <DAO.h>
+
+namespace ViewModels {
+
+class CustomersViewModel : public QAbstractTableModel
+{
+public:
+    explicit CustomersViewModel(QObject* parent = 0);
+    virtual ~CustomersViewModel();
+
+    virtual auto columnCount(const QModelIndex& parent) const -> int override;
+    virtual auto rowCount(const QModelIndex& parent) const -> int override;
+    virtual auto data(const QModelIndex& index, int role) const -> QVariant override;
+    virtual auto flags(const QModelIndex& index) const -> Qt::ItemFlags override;
+    virtual auto headerData(int section, Qt::Orientation orientation, int role) const -> QVariant override;
+
+private:
+    QList<Entities::Customer> customers;
+
+};
+
+} // namespace ViewModels
---
+++ Qt-Widgets-Examples/QTableView-QSqlDatabase-Example/src/view/MainWindow.cpp
@@ -0,0 +1,53 @@
+#include "MainWindow.h"
+#include "./ui_MainWindow.h"
+
+MainWindow::MainWindow(QWidget* parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    customersViewModel = std::make_shared<ViewModels::CustomersViewModel>();
+    productsViewModel = std::make_shared<ViewModels::ProductsViewModel>();
+    ordersViewModel = std::make_shared<ViewModels::OrdersViewModel>();
+
+    ui->customersTableView->setModel(customersViewModel.get());
+    ui->productsTableView->setModel(productsViewModel.get());
+    ui->ordersTableView->setModel(ordersViewModel.get());
+
+    ui->customersTableView->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
+    ui->productsTableView->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
+    ui->ordersTableView->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+/*auto MainWindow::updateCustomer() -> void
+{
+    Entities::Customer customer;
+
+    customer.setId(4);
+    customer.setFirstName("Ron");
+    customer.setLastName("Tramp");
+    customer.setEmail("ron_t_25@gmail.com");
+    customer.setAge(25);
+
+    DAO::insertCustomer(customer);
+}
+
+auto MainWindow::updateProduct() -> void
+{
+    Entities::Product product;
+
+    product.setId(6);
+    product.setType("Phone");
+    product.setName("iPhone 16");
+    product.setManufacturer("Gorizont Company");
+    product.setCount(5);
+    product.setPrice(99000);
+
+    DAO::updateProduct(product);
+}*/
