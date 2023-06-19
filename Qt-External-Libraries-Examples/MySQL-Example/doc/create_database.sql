a0249be6eca8f28a4f4ba5ef3d3e44b0b3cabf4a
DESKTOP-3N8RAQ7\Сергей <tyulenev.i@yandex.ru>
2023-06-19 15:37:58 +0300

    Modified MySQL-Example

--- Qt-External-Libraries-Examples/MySQL-Example/doc/create_database.sql
+++ Qt-External-Libraries-Examples/MySQL-Example/doc/create_database.sql
@@ -1,18 +1,3 @@
-# MySql-Example
-
-## Описание
-
-Пример использования библиотеки libmysql для взаимодействия с базами данных MySQL.
-Данные выводятся через view-model в QTableView. Используется пул соединений.
-
-![alt text](doc/MySQL-Example.png)
-
-## Сборка проекта
-
-1. Скачать и установить MySQL (+ стандартный MySQL Connector C++).
-2. Сгенерировать базу данных (или doc/create_database.sql):
-
-``` sql
 CREATE DATABASE test_db;
 
 USE test_db;
@@ -61,38 +46,3 @@ INSERT INTO Products VALUES (default, 'Laptop', 'V15', 'Lenovo', 6, 46000);
 INSERT INTO Orders VALUES (default, 1, 1, 2, '2022-01-08', '12:21:34');
 INSERT INTO Orders VALUES (default, 2, 3, 1, '2022-01-04', '10:41:54');
 INSERT INTO Orders VALUES (default, 3, 5, 1, '2022-01-12', '16:09:12');
-```
-3. Собрать проект (cборку можно производить из QtCreator или из папки build коммандами):
-
-```
-cmake ..
-make
-```
-или
-
-```
-qmake ../-project
-qmake ..
-make
-```
-
-## Версии
-
-Версии сред, языков и утилит, которые использовались на момент написания проекта.
-
-| Название   | Версия               |
-| -----------|----------------------|
-| C++        | 20                   |
-| Qt         | 6.4.0                |
-| CMake      | 3.5                  |
-| QMake      | 6.0.1 (6.0.0)        |
-| MinGW      | 9.0.0 64 bit         |
-| MySQL      | 8.0                  |
-
-> MinGW 9.0.0 64 bit может не найти libmysql.dll в QMake сборке, необходимо проверять пути.
-
-## Тестирование
-
-- [x] Windows (10, сборка 19044.2604)
-- [ ] Linux
-- [ ] Mac
