513bfd6746f134cdc01cf68dc5972085d942475a
DESKTOP-3N8RAQ7\Сергей <tyulenev.i@yandex.ru>
2023-07-05 13:29:35 +0300

    Update CPack-Example

--- Qt-External-Libraries-Examples/CPack-Example/main.cpp
+++ Qt-External-Libraries-Examples/CPack-Example/main.cpp
@@ -1,11 +1,13 @@
-#include "mainwindow.h"
+#include "MainWindow.h"
 
 #include <QApplication>
 
-int main(int argc, char *argv[])
+auto main(int argc, char* argv[]) -> int
 {
     QApplication a(argc, argv);
+
     MainWindow w;
     w.show();
+
     return a.exec();
 }
