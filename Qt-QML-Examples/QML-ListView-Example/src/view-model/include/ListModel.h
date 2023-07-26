bf36cdef7aacafd95285b0c2c68d25701b80112a
DESKTOP-3N8RAQ7\Сергей <tyulenev.i@yandex.ru>
2023-07-26 17:09:10 +0300

    Minor changes in QML-ListView-Example. Updates CMakeLists in other projects with MVD

--- Qt-QML-Examples/QML-ListView-Example/src/view-model/include/ListModel.h
+++ Qt-QML-Examples/QML-ListView-Example/src/view-model/include/ListModel.h
@@ -4,27 +4,33 @@
 
 namespace ViewModels {
 
-struct TestModel;
-
 class ListModel : public QAbstractListModel
 {
-public:
-    explicit ListModel(QObject* parent = 0);
-    virtual ~ListModel();
-
-    auto rowCount(const QModelIndex& parent) const -> int override;
-    auto data(const QModelIndex& index, int role) const -> QVariant override;
-    auto flags(const QModelIndex& index) const -> Qt::ItemFlags override;
-    auto roleNames() const -> QHash<int, QByteArray> override;
 
 private:
+    struct TestModel {
+        QString name;
+        QString iconUrl;
+        QString description;
+        bool status;
+    };
+
     QList<TestModel> model;
+
+public:
+    explicit ListModel(QObject* parent = 0);
+    virtual ~ListModel() = default;
 
 protected:
     enum Role {
         DescriptionRole = Qt::UserRole + 1,
         StatusRole
     };
+
+    virtual auto rowCount(const QModelIndex& parent) const -> int override;
+    virtual auto data(const QModelIndex& index, int role) const -> QVariant override;
+    virtual auto flags(const QModelIndex& index) const -> Qt::ItemFlags override;
+    virtual auto roleNames() const -> QHash<int, QByteArray> override;
 
 };
 
