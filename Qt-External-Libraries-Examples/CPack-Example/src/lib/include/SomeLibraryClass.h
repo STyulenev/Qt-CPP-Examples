7870b16f19b3eeca41c5655dfea70e830dde090e
Sergey Tyulenev <tyulenev.i@yandex.ru>
2024-05-29 17:49:39 +0300

    Update

---
+++ Qt-External-Libraries-Examples/CPack-Example/src/lib/include/SomeLibraryClass.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <QString>
+
+namespace cpack_lib {
+
+QString getTestText();
+
+} // namespace cpack_lib
---
+++ Qt-External-Libraries-Examples/CPack-Example/src/lib/src/SomeLibraryClass.cpp
@@ -0,0 +1,10 @@
+#include "SomeLibraryClass.h"
+
+namespace cpack_lib {
+
+QString getTestText()
+{
+    return QString("Text from cpack_lib");
+}
+
+} // namespace cpack_lib
